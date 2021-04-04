32df48b85604705e0219fbaf8ca0a3db262424f4
Flexfreshl <stopmeifuckan@yandex.ru>
2021-04-04 22:47:58 +0700

    added product (mixitup,fancybox)

--- app/js/main.js
+++ app/js/main.js
@@ -6,4 +6,15 @@ $(function() {
         autoplay: true,
         autoplayspeed: 2000,
     });
+
+    $('.product__item').hover(function(event) {
+        $(this).find('.product__images').show();
+    }, function() {
+        $(this).find('.product__images').hide();
+    });
+
+
+
+    var mixer = mixitup('.product__items');
 });
+// id="MixItUpAFD5FC"
